25d25ff63492b912a4454d86cf977c502c749389
srMarlins <srmarlins@gmail.com>
2026-02-25 08:32:16 -0500

    Wire Koin UI module and app integration (#12)

--- android/app/src/main/kotlin/com/chromadmx/android/ChromaDMXApp.kt
+++ android/app/src/main/kotlin/com/chromadmx/android/ChromaDMXApp.kt
@@ -2,6 +2,7 @@ package com.chromadmx.android
 
 import android.app.Application
 import com.chromadmx.android.di.appModule
+import com.chromadmx.ui.di.uiModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -18,7 +19,7 @@ class ChromaDMXApp : Application() {
         startKoin {
             androidLogger(Level.ERROR)
             androidContext(this@ChromaDMXApp)
-            modules(appModule)
+            modules(appModule, uiModule)
         }
     }
 }
