73cfd8705ce3b0542c31c7cfb0fb697282dc1782
srMarlins <srmarlins@gmail.com>
2026-02-25 00:31:04 -0500

    Add ios/ and build-logic/ placeholder structure
    Create placeholder directories for future development:
    - ios/ with .gitkeep (Xcode project will be set up later)
    - build-logic/ with settings.gradle.kts, convention module scaffold,
      and source directory structure (to be populated in Issue #2)
    
    Part of #1

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    `kotlin-dsl`
+}
+
+dependencies {
+    // Convention plugin dependencies will be added in Issue #2
+    // compileOnly(libs.android.gradlePlugin)
+    // compileOnly(libs.kotlin.gradlePlugin)
+    // compileOnly(libs.compose.gradlePlugin)
+}
